dc4f752791eda2f96860abc832ce7b293c63ab1b
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2023-05-08 12:09:14 +0200

    lookAt config

--- sqrt-bash/src/main.cpp
+++ sqrt-bash/src/main.cpp
@@ -14,6 +14,7 @@
 #include <array>
 #include "projection.cpp"
 
+
 int main() {
     if (!glfwInit()) {
         std::cout << "GLFW was not initialized\n";
@@ -46,40 +47,22 @@ int main() {
 
     // --------------------------
     float positions[] = {
-        0.f, 1.f, 0.f, 0.01f,
-        1.f, 1.f, 0.f, 0.02f,
-        1.f, 0.f, 0.f, 0.03f,
-        0.f, 0.f, 0.f, 0.04f,
+        0.f, 1.f, -0.1f, 0.01f,
+        1.f, 1.f, -0.1f, 0.02f,
+        1.f, 0.f, -0.1f, 0.03f,
+        0.f, 0.f, -0.1f, 0.04f,
         0.f, 1.f, 0.1f, 0.05f, 
         1.f, 1.f, 0.1f, 0.06f,
         1.f, 0.f, 0.1f, 0.07f,
         0.f, 0.f, 0.1f, 0.09f
     };
-    float aositions[] = {
-        0.f, 1.f, 0.f,
-        1.f, 1.f, 0.f,
-        1.f, 0.f, 0.f,
-        0.f, 0.f, 0.f,
-        0.f, 1.f, 0.1f, 
-        1.f, 1.f, 0.1f,
-        1.f, 0.f, 0.1f,
-        0.f, 0.f, 0.1f
-    };
-    for (int i = 0; i < sizeof(positions) / sizeof(float); i++) positions[i] *= 10.f;
+   for (int i = 0; i < sizeof(positions) / sizeof(float); i++) positions[i] *= 10.f;
 
     unsigned int indices[] = {
         0, 1, 2,
-        0, 2, 3,
-        7, 5, 4,
-        7, 6, 5,
-        3, 2, 5,
-        3, 5, 6,
-        2, 1, 4,
-        2, 4, 5,
-        0, 7, 6,
-        0, 6, 4,
-        0, 1, 4,
-        0, 7, 4
+        0, 2, 3,        
+        4, 5, 6,
+        4, 6, 7,       
     };
     // -------------------------
 
@@ -119,24 +102,16 @@ int main() {
 
 // ---------------
     float fov = 90.f;
-    float near = .1f;
-    float far = 100.0f;
-    float zCam = -14.f;
-    glm::mat4 prj = glm::ortho(0.f, 800.f, 0.f, 600.f, near, far);
-    glm::mat4 model = glm::mat4(1.0f); // Initialize as identity matrix for this example
-    glm::mat4 projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, near, far); // Example perspective projection
-    //glm::vec4 viewport = glm::vec4(0.0f, 0.0f, 800.0f, 600.0f); // Example viewport of size 800x600
-    glm::mat4 viewport = glm::lookAt(
-        glm::vec3(0, 0, zCam), // Camera is at (4,3,3), in World Space
-        glm::vec3(0, 0, 0), // and looks at the origin
-        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-    
+    float near = .1f, far = 100.0f;
     float x_angle = 0.0f;
-    float y_angle = 0.0f;  // rotate 45 degrees around the y-axis
+    float y_angle = 0.0f;    
+    float xc(4), yc(3), zc(3);
+    float xla(0), yla(0), zla(1);
+    glm::mat4 x_rotation, y_rotation;
 
-    glm::mat4 x_rotation = glm::rotate(glm::mat4(1.0f), glm::radians(x_angle), glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 y_rotation = glm::rotate(glm::mat4(1.0f), glm::radians(y_angle), glm::vec3(1.0f, 1.0f, 0.0f));
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, near, far); 
+    glm::mat4 viewport;
 // ------------------
         
         while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
@@ -144,25 +119,33 @@ int main() {
             glClear(GL_COLOR_BUFFER_BIT);
             ImGui_ImplGlfwGL3_NewFrame();
 
-            {
+            {   
+                ImGui::SliderFloat("xc", &xc, -15.f, 15.f);
+                ImGui::SliderFloat("yc", &yc, -15.f, 15.f);
+                ImGui::SliderFloat("zc", &zc, -15.f, 15.f);
+
+                ImGui::SliderFloat("xla", &xla, -15.f, 15.f);
+                ImGui::SliderFloat("yla", &yla, -15.f, 15.f);
+                ImGui::SliderFloat("zla", &zla, -15.f, 15.f);
+             
                 ImGui::SliderFloat("XAngle", &x_angle, 0.f, 360.f);
                 ImGui::SliderFloat("YAngle", &y_angle, 0.f, 360.f);
-                ImGui::SliderFloat("zCam", &zCam, -10.f, 10.f);
-                ImGui::SliderFloat("Red", &color[0], 0.f, 1.f);
                 ImGui::SliderFloat("FOV", &fov, 0.0f, 360.0f);
-                ImGui::SliderFloat("near", &near, -2.0f, 2.0f);
-                ImGui::SliderFloat("far", &far, -100.0f, 1000.0f);
+                //ImGui::SliderFloat("near", &near, -2.0f, 2.0f);
+                //ImGui::SliderFloat("far", &far, -100.0f, 1000.0f);
                 ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
              viewport = glm::lookAt(
-                glm::vec3(0, 0, zCam), // Camera is at (4,3,3), in World Space
-                glm::vec3(0, 0, 0), // and looks at the origin
-                glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+                glm::vec3(xc, yc, zc),
+                glm::vec3(xla, yla, zla),
+                glm::vec3(0, 1, 0)
             );
+
             x_rotation = glm::rotate(glm::mat4(1.0f), glm::radians(x_angle), glm::vec3(1.0f, 0.0f, 0.0f));
             y_rotation = glm::rotate(glm::mat4(1.0f), glm::radians(y_angle), glm::vec3(0.0f, 1.0f, 0.0f));
             projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, near, far)*viewport*x_rotation*y_rotation;  
+            
             glUniformMatrix4fv(uniformMtx, 1, GL_FALSE, glm::value_ptr(projection));
             glUniform4f(uniformColor, color[0], color[1], color[2], color[3]);
             
